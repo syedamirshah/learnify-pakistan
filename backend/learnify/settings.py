04e76a912ce89a90c30d09ddd4b842e67743a4d1
syedamirshah <syedamirshah@gmail.com>
2025-08-01 09:12:30 +0500

    Update settings.py

--- backend/learnify/settings.py
+++ backend/learnify/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!2nszdij8sl0oljn=f)wd2uowab_ae@mm))r0=6#si8ozke&sy'
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-!2nszdij8sl0oljn=f)wd2uowab_ae@mm))r0=6#si8ozke&sy")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
